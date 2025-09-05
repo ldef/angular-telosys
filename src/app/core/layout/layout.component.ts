7a52ba3bf0b9fad8f057b15661d42229c5c8104c
Lilian Defarges <lilian.defarges@capgemini.com>
2025-09-05 08:34:40 +0200

    Bootstrap layout

--- src/app/core/layout/layout.component.ts
+++ src/app/core/layout/layout.component.ts
@@ -1,13 +1,20 @@
-import { Component, inject, ChangeDetectionStrategy } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-
+import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-layout',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class LayoutComponent {
+  menu = [
+    { label: 'Accueil', icon: '📊', link: '/' },
+    { label: 'Projets', icon: '📁', link: '/projects' },
+    { label: 'Générateurs', icon: '⚙️', link: '/generators' },
+    { label: 'Modèles', icon: '👥', link: '/models' },
+    { label: 'Rapports', icon: '📈', link: '/reports' },
+    { label: 'Intégrations', icon: '🔗', link: '/integrations' }
+  ];
 }
